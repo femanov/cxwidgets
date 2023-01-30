bd279ed6cb5d43ad70613035f813c7b3007a5722
femanov <femanov@gmail.com>
2023-01-30 15:36:54 +0700

    some sizes correction

--- cxwidgets/__init__.py
+++ cxwidgets/__init__.py
@@ -36,4 +36,4 @@ __all__ = [HLine, VLine, BaseGridW, BaseFrameGridW, PCheckBox, PComboBox, PDoubl
            CXScrollPlotDataItem, TimeAxisItem, AgeAxisItem
            ]
 
-__version__ = "0.15"
+__version__ = "0.16"
